18d289ec53634bc54368f0b2599ce3d8b539e399
doris52k <doris52k@gmail.com>
2016-04-03 17:39:48 +0900

    1

---
+++ footer.php
@@ -0,0 +1,10 @@
+			<div id="footer">
+				<span class="copyright">
+					m.&nbsp;&nbsp;+82&nbsp;10&nbsp;9258&nbsp;9229
+					<br>e.&nbsp;&nbsp;doris52k@gmail.com
+					<br>&#169;<?php bloginfo('name'); ?>
+				</span>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="<?php bloginfo('charset'); ?>">
+<meta name="viewport" content="width=device-width, maximum-scale=1.0, initial-scale=1.0, user-scalable=no">
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquery-1.11.1.min.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/pageControl.js"></script>
+<title><?php bloginfo('name'); ?> <?php wp_title('|'); ?></title>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+<link href='https://fonts.googleapis.com/css?family=Dosis:200,300,400,500,600,700,800' rel='stylesheet' type='text/css'>
+</head>
+
+<body <?php body_class(); ?>>
+	<div id="container">
