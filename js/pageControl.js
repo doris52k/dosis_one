4c8b4469afd30c9109166982e8f9d6ba0a589f38
bee2side <bee2side@gmail.com>
2016-05-06 18:56:05 +0900

    1

--- js/pageControl.js
+++ js/pageControl.js
@@ -4,12 +4,14 @@ $(document).ready(function(){
 		if (topMenuNum == 0) {
 			$(".blogTitle").css("visibility", "hidden");
 			$(".singleTitle").css("visibility", "hidden");
+			$(".backbtn").css("visibility", "hidden");
 			$(".top_menu").css("display", "block");
 			$("#menubtn_id").attr('class', 'menubtn_close')
 			topMenuNum = 1;
 		}else{
 			$(".blogTitle").css("visibility", "visible");
 			$(".singleTitle").css("visibility", "visible");
+			$(".backbtn").css("visibility", "visible");
 			$(".top_menu").css("display", "none");
 			$("#menubtn_id").attr('class', 'menubtn')
 			topMenuNum = 0;
