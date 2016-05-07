9bd1f4f70897a75ff0349c23b04cd29b2cd412fb
bee2side <bee2side@gmail.com>
2016-05-07 18:42:52 +0900

    1

--- js/pageControl.js
+++ js/pageControl.js
@@ -2,15 +2,11 @@ $(document).ready(function(){
 	var topMenuNum = 0
 	function topMenuSwap(){
 		if (topMenuNum == 0) {
-			$(".blogTitle").css("visibility", "hidden");
-			$(".singleTitle").css("visibility", "hidden");
 			$(".backbtn").css("visibility", "hidden");
 			$(".top_menu").css("display", "block");
 			$("#menubtn_id").attr('class', 'categoryButton_close')
 			topMenuNum = 1;
 		}else{
-			$(".blogTitle").css("visibility", "visible");
-			$(".singleTitle").css("visibility", "visible");
 			$(".backbtn").css("visibility", "visible");
 			$(".top_menu").css("display", "none");
 			$("#menubtn_id").attr('class', 'categoryButton')
@@ -21,7 +17,3 @@ $(document).ready(function(){
 		topMenuSwap();
 	});
 });
-$(window).load(function(){
-	 $('.categoryButton_bar_one').css("transition-duration", "0.3s")
-	 $('.categoryButton_bar_two').css("transition-duration", "0.3s")
-})
