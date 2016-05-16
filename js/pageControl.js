64ef8c62fe445ce9f66895aaf9d043fee3f83604
bee2side <bee2side@gmail.com>
2016-05-16 21:24:46 +0900

    1

--- js/pageControl.js
+++ js/pageControl.js
@@ -6,12 +6,14 @@ $(document).ready(function(){
 			$(".top_menu").css("display", "block");
 			$("#menubtn_id").attr('class', 'categoryButton_close')
 			$("#head_column").attr('class', 'top_column_close')
+			$("#header").attr('class', 'header_close')
 			topMenuNum = 1;
 		}else{
 			$(".singleTitle p").css("visibility", "visible");
 			$(".top_menu").css("display", "none");
 			$("#menubtn_id").attr('class', 'categoryButton')
 			$("#head_column").attr('class', 'top_column')
+			$("#header").attr('class', '')
 			topMenuNum = 0;
 		};
 	};
